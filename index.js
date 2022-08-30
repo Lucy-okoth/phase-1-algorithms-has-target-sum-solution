f9690eed60c80d3884f63a80a21e7673e8635655
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-08-30 22:44:24 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+     if (seenNumbers[complement]) return true;
+     seenNumbers[array[i]] = true;
+  }
+return false;
 }
+
+hasTargetSum([20,35,25], 50);
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: 0(n)
 */
 
 /* 
   Add your pseudocode here
+  1. hasTargetSum(array, target)
+  2. iterates throught each number in the array
+  3. Add an object which stores the keys of the seen numbers
+  4. for the current num identify a complement that adds upto the target value
+  5. iterate throught the array again
+  6. if the value achieved is our complement return true 
+  7. else if its not achieved return false
 */
-
 /*
   Add written explanation of your solution here
 */
@@ -31,4 +47,4 @@ if (require.main === module) {
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
-module.exports = hasTargetSum;
+module.exports = hasTargetSum; 
